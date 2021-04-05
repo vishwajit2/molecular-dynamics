635fef3b7226f0ddd89b989bfc1eeda9f42a60c7
Vishwajit Kadam <vishwajitkadam13@gmail.com>
2021-04-05 19:29:37 +0530

    added cross platform sleep (windows and ubuntu)

--- src/utilities.h
+++ src/utilities.h
@@ -1,16 +1,19 @@
-// Assert ptr is not a null pointer
-#ifndef CHECKER_H
-#define CHECKER_H
+#ifndef UTILITY_H
+#define UTILITY_H
+
 #include <stdio.h>
+
+void sleep_ms(int milliseconds);
+
 #define NP_CHECK(ptr)                                   \
     {                                                   \
-        if (NULL == (ptr)) {                            \
+        if (NULL == (ptr))                              \
+        {                                               \
             fprintf(stderr, "%s:%d NULL POINTER: %s n", \
                     __FILE__, __LINE__, #ptr);          \
             exit(-1);                                   \
         }                                               \
     }
-
 // for debugging
 #define DEBUG(msg) fprintf(stderr, "%s:%d %s", __FILE__, __LINE__, (msg))
 
